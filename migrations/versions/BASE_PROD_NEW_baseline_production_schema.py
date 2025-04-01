a2dad8f55528121269bce6972431db9790f54e88
nastradacha <nastradacha@hotmail.com>
2025-04-01 18:08:40 -0400

    BASE_PROD_NEW

---
+++ migrations/versions/BASE_PROD_NEW_baseline_production_schema.py
@@ -0,0 +1,24 @@
+"""Baseline production schema
+
+Revision ID: BASE_PROD_NEW
+Revises: bf626dab411e
+Create Date: 2025-04-01 18:03:13.736718
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'BASE_PROD_NEW'
+down_revision = 'bf626dab411e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
