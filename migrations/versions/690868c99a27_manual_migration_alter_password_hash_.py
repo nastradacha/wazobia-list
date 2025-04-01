5f90d2ce13bf921e7b0e0ec7663ac7160549b96a
nastradacha <nastradacha@hotmail.com>
2025-04-01 15:08:08 -0400

    posting ads after lgin, editing and deleting add

--- migrations/versions/690868c99a27_manual_migration_alter_password_hash_.py
+++ migrations/versions/690868c99a27_manual_migration_alter_password_hash_.py
@@ -8,23 +8,15 @@ Create Date: 2025-04-01 14:56:52.689603
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision = '690868c99a27'
 down_revision = '40c72f2621ef'
 branch_labels = None
 depends_on = None
 
-
 def upgrade():
-    op.alter_column('user', 'password_hash',
-                    existing_type=sa.String(length=128),
-                    type_=sa.String(length=256),
-                    existing_nullable=False)
-
+    # For PostgreSQL, use raw SQL to alter the column type.
+    op.execute('ALTER TABLE "user" ALTER COLUMN password_hash TYPE VARCHAR(256);')
 
 def downgrade():
-    op.alter_column('user', 'password_hash',
-                    existing_type=sa.String(length=256),
-                    type_=sa.String(length=128),
-                    existing_nullable=False)
+    op.execute('ALTER TABLE "user" ALTER COLUMN password_hash TYPE VARCHAR(128);')
