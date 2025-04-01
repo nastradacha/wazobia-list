8935e4fa2df7b773bd7dc0bfbb0335d198687999
nastradacha <nastradacha@hotmail.com>
2025-04-01 17:47:28 -0400

    4fe4573209e8

---
+++ migrations/versions/4fe4573209e8_baseline_migration_for_production.py
@@ -0,0 +1,24 @@
+"""Baseline migration for production
+
+Revision ID: 4fe4573209e8
+Revises: 690868c99a27
+Create Date: 2025-04-01 17:45:10.363145
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4fe4573209e8'
+down_revision = '690868c99a27'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
