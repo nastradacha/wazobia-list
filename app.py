4979089163730530b8041dcc66c2524bd32ba888
nastradacha <nastradacha@hotmail.com>
2025-04-03 14:09:35 -0400

    added email otp bug fix

--- app.py
+++ app.py
@@ -475,6 +475,8 @@ def load_user(user_id):
 #      Application Entry        #
 # ---------------------------- #
 
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
+   
     app.run()
